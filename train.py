dc72cb9a7776d7f977026c38f8e8fd05f11b02fc
UCIHARadded <deadliestrishabh@gmail.com>
2025-06-21 20:46:09 +0530

    Update train.py

--- train.py
+++ train.py
@@ -9,7 +9,8 @@ from alg.opt import *
 from alg import alg, modelopera
 from utils.util import set_random_seed, get_args, print_row, print_args, train_valid_target_eval_names, alg_loss_dict, print_environ, disable_inplace_relu
 from datautil.getdataloader_single import get_act_dataloader
-from torch.utils.data import DataLoader, ConcatDataset, get_curriculum_loader
+from torch.utils.data import DataLoader, ConcatDataset
+from datautil.getcurriculumloader import get_curriculum_loader
 from network.act_network import ActNetwork
 from sklearn.metrics import davies_bouldin_score
 # Unified SHAP utilities import

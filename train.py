f14d9a55fc7bf553ef37d1b851d288f1a69ed42e
rishabharizona <149159763+rishabharizona@users.noreply.github.com>
2025-06-25 14:21:46 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,3 @@
-# Suppress TensorFlow and SHAP warnings
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow logging
-import tensorflow as tf
-tf.get_logger().setLevel('ERROR')  # Suppress TensorFlow warnings
-import logging
-logging.getLogger("shap").setLevel(logging.WARNING)  # Suppress SHAP warnings
 import os
 import sys
 import subprocess
@@ -25,6 +18,13 @@ from torch_geometric.loader import DataLoader
 from torch.utils.data import ConcatDataset
 from network.act_network import ActNetwork
 
+# Suppress TensorFlow and SHAP warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow logging
+import tensorflow as tf
+tf.get_logger().setLevel('ERROR')  # Suppress TensorFlow warnings
+import logging
+logging.getLogger("shap").setLevel(logging.WARNING)  # Suppress SHAP warnings
 # Unified SHAP utilities import
 from shap_utils import (
     get_background_batch, safe_compute_shap_values, plot_summary,
